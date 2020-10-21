d43259b1782c683dd1d4fb1bbbd28249780fd7a9
jgironfo <jgironfo@MAD-CYWDK13.usersad.everis.int>
2020-10-21 12:13:34 +0200

    WZ_TC_0005 - Se añade el caso 5

---
+++ src/test/java/Test/WZ_TC_0005.java
@@ -0,0 +1,77 @@
+package Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.testng.annotations.Optional;
+
+import io.qameta.allure.Description;
+import pageibject.Login;
+import pageibject.NumeroTarjeta;
+import general.Browser;
+
+import general.Final;
+import general.Log;
+import general.Reporting;
+
+
+
+public class WZ_TC_0005 {
+	
+	@Parameters({ "browserName","userName","password"})
+	@Test()
+	@Description("Mostrar número tarjeta")
+	static void login(@Optional (Final.CHROME) String browserName, @Optional String userName , @Optional  String pass) throws Exception {
+
+		String codeTC = Browser.getActualTC(Thread.currentThread().getStackTrace()[Final.ONE].getClassName());
+
+		try {
+
+			// We launch the Test case
+			Reporting.initializeReporting(codeTC);
+			Reporting.reportOK("*** EJECUCIÓN PRUEBA: " + codeTC + " ***");
+
+			/**
+			 * INITIALIZE THE OBJECTS
+			 * 
+			 */
+
+			Login login = new Login();
+			NumeroTarjeta numeroTarjeta = new NumeroTarjeta();
+		
+			
+			/**
+			 *  INITIALIZE BROWSER
+			*/
+			Browser.start();
+			
+			/**
+			 * NAVIGATE TO URL
+			 */
+			
+			Browser.navegar();
+			
+			/**
+			 * Start test
+			 */
+			
+			login.doLoginUsuarioPassword(userName,pass);
+			numeroTarjeta.mostrarNumeroTarjeta();
+			Reporting.reportResultOK();
+
+		} catch (Exception e) {
+			Reporting.reportResultKO();
+			Log.info(e);
+			
+		} finally {
+			Browser.stopDriver();
+			Log.info("Cerramos el navegador");
+		}
+
+
+	}
+
+}
