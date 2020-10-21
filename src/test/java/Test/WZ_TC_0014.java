86412ae2f05dd8aca95e0b41f8ffc74dc5a83cd7
jgironfo <jgironfo@MAD-CYWDK13.usersad.everis.int>
2020-10-21 15:18:55 +0200

    Subida de los casos del 10 al 21

---
+++ src/test/java/Test/WZ_TC_0014.java
@@ -0,0 +1,78 @@
+package Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.testng.annotations.Optional;
+
+import io.qameta.allure.Description;
+import pageibject.AreaPersonal;
+import pageibject.Login;
+import pageibject.Mensajes;
+import general.Browser;
+
+import general.Final;
+import general.Log;
+import general.Reporting;
+
+
+
+public class WZ_TC_0014 {
+	
+	@Parameters({ "browserName","userName","password"})
+	@Test()
+	@Description("Cambiar Nombre Usuario")
+	static void login(@Optional (Final.CHROME) String browserName, @Optional String userName , @Optional  String pass) throws Exception {
+
+		String codeTC = Browser.getActualTC(Thread.currentThread().getStackTrace()[Final.ONE].getClassName());
+
+		try {
+
+			// We launch the Test case
+			Reporting.initializeReporting(codeTC);
+			Reporting.reportOK("*** EJECUCIÓN PRUEBA: " + codeTC + " ***");
+
+			/**
+			 * INITIALIZE THE OBJECTS
+			 * 
+			 */
+
+			Login login = new Login();
+			AreaPersonal areaPersonal = new AreaPersonal();
+			
+			/**
+			 *  INITIALIZE BROWSER
+			*/
+			Browser.start();
+			
+			/**
+			 * NAVIGATE TO URL
+			 */
+			
+			Browser.navegar();
+			
+			/**
+			 * Start test
+			 */
+			
+			login.doLoginUsuarioPassword(userName, pass);
+			//PENDIENTE
+			//areaPersonal.cambiarUsuarioPerfilAreaPersonal();
+			Reporting.reportResultOK();
+
+		} catch (Exception e) {
+			Reporting.reportResultKO();
+			Log.info(e);
+			
+		} finally {
+			Browser.stopDriver();
+			Log.info("Cerramos el navegador");
+		}
+
+
+	}
+
+}
---
+++ src/test/java/Test/WZ_TC_0021.java
@@ -0,0 +1,79 @@
+package Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.testng.annotations.Optional;
+
+import io.qameta.allure.Description;
+import pageibject.AreaPersonal;
+import pageibject.Login;
+import pageibject.Mensajes;
+import pageibject.PosicionGlobal;
+import general.Browser;
+
+import general.Final;
+import general.Log;
+import general.Reporting;
+
+
+
+public class WZ_TC_0021 {
+	
+	@Parameters({ "browserName","userName","password"})
+	@Test()
+	@Description("Visualizar Gráfica de gastos del mes")
+	static void login(@Optional (Final.CHROME) String browserName, @Optional String userName , @Optional  String pass) throws Exception {
+
+		String codeTC = Browser.getActualTC(Thread.currentThread().getStackTrace()[Final.ONE].getClassName());
+
+		try {
+
+			// We launch the Test case
+			Reporting.initializeReporting(codeTC);
+			Reporting.reportOK("*** EJECUCIÓN PRUEBA: " + codeTC + " ***");
+
+			/**
+			 * INITIALIZE THE OBJECTS
+			 * 
+			 */
+
+			Login login = new Login();
+			Producto producto = new Producto();
+			
+			/**
+			 *  INITIALIZE BROWSER
+			*/
+			Browser.start();
+			
+			/**
+			 * NAVIGATE TO URL
+			 */
+			
+			Browser.navegar();
+			
+			/**
+			 * Start test
+			 */
+			
+			login.doLoginUsuarioPassword(userName, pass);
+			//PENDIENTE
+			//producto.checkProductoExtractosGraficos();
+			Reporting.reportResultOK();
+
+		} catch (Exception e) {
+			Reporting.reportResultKO();
+			Log.info(e);
+			
+		} finally {
+			Browser.stopDriver();
+			Log.info("Cerramos el navegador");
+		}
+
+
+	}
+
+}
