68b36dbec261a8d47ce8a29bbab520047078e3f8
cexmobile <cexmobile@192.168.1.71>
2021-05-10 09:58:53 +0200

    Cambiado boton datos demográficos

--- src/main/java/pageobject/AreaPersonal.java
+++ src/main/java/pageobject/AreaPersonal.java
@@ -798,11 +798,11 @@ public class AreaPersonal extends Utilidades {
 			Reporting.reportOK("OK - Se pulsa en el botn 'rea Personal'");
 
 			// 1.4 Pulsar el botn Cambiar Datos Demogrficos
-			Browser.waitForElementScreen(btnCambiarDatoDemografico);
+			Browser.waitForElementScreen(btnCambiarDatosAreaPersonal);
 			try { this.takeRemoteScreenshot(driver, codeTC); } catch(Exception e){
 				  e.printStackTrace(); }
 			Utilidades.addImagesToWordDocument(doc, Utilidades.fileGetRemoteScreenshot(driver));
-			Browser.clickElementSyncro(btnCambiarDatoDemografico);
+			Browser.clickElementSyncro(btnCambiarDatosAreaPersonal);
 			//egea.reportaTraza(testCase, "INFO", "OK", "Se pulsa sobre el link 'Cambiar Datos Demogrficos'", "");
 			Reporting.reportOK("OK - Se pulsa sobre el link 'Cambiar Datos Demogrficos'");
 
